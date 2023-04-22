4373c7aa693c22b27f862a3208c42f397f9b9ade
PedroCamillozzi <pedrocamillozzi01@gmail.com>
2023-04-22 00:15:44 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaPensarEj3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int p=1, po=0, q, n;
+            Console.WriteLine("¿Cuantos numeros quiere calcular para la suma de Fibonnacci?");
+            q = Convert.ToInt32(Console.ReadLine());
+            for (int i=3; i<q; i++)
+            {
+                n = (p + po);
+                po = p;
+                p = n;
+                Console.WriteLine(n.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
